07c9c88027e93c3d4a32ab82b79e090ca6d826b9
Ruben <Ruben@Ruben>
2025-03-11 13:24:20 +0100

    commit  a

--- src/olimpiadas/principal.java
+++ src/olimpiadas/principal.java
@@ -14,7 +14,7 @@ public class principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        
     }
     
 }
