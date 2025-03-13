01d1ee341b55b921f8f40ca63cb638cd2ce2745b
eduar <eduar@EduPico25>
2025-03-13 08:48:10 +0100

    primero edu

--- src/olimpiadas/principal.java
+++ src/olimpiadas/principal.java
@@ -14,6 +14,7 @@ public class principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Me me meo ");
         
     }
     
