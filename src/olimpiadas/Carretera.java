8df3f369efc9119d7f5539c45e95986c0708fae7
Ruben <Ruben@Ruben>
2025-03-19 10:25:15 +0100

    commit empezando carretera y mapa

--- src/olimpiadas/Carretera.java
+++ src/olimpiadas/Carretera.java
@@ -9,5 +9,20 @@ package olimpiadas;
  * @author eduar
  */
 public class Carretera {
+    private int inicioX;
+    private int finalX;
+    private int inicioY;
+    private int finalY;
+    private boolean esVertical;
+
+    public Carretera(int inicioX, int finalX, int inicioY, int finalY, boolean esVertical) {
+        this.inicioX = inicioX;
+        this.finalX = finalX;
+        this.inicioY = inicioY;
+        this.finalY = finalY;
+        this.esVertical = esVertical;
+    }
+    
+    
     
 }
