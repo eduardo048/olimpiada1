b131bc67944ee28b0f71cfb988e9d4da3590843b
eduar <eduar@EduPico25>
2025-03-19 12:22:45 +0100

    cambio mapa

--- src/olimpiadas/Mapa.java
+++ src/olimpiadas/Mapa.java
@@ -1,6 +1,9 @@
 
 package olimpiadas;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author eduar
@@ -9,24 +12,31 @@ public class Mapa {
     private int tamaño;
     private int numCarreteraVer;
     private int numCarreteraOri; 
-    private int cruces;
     private int numeroVehiculo;
-
+    private ArrayList<Carretera> carreteras;
+    
     public Mapa() {
     }
 
-    public Mapa(int tamaño, int numeroVehiculo ) {
+    public Mapa(int tamaño, int numeroVehiculo, int numeroCarreteras ) {
         this.tamaño = tamaño;
-        
         this.numeroVehiculo = numeroVehiculo;
+        this.carreteras = new ArrayList<>();
+        calcularCarretera(numeroCarreteras);
     }
     
-    public void calcularCarretera(int numeroCarreteras){
-        
+    private void calcularCarretera(int numeroCarreteras){
         this.numCarreteraOri=numeroCarreteras/2;
         this.numCarreteraVer=numeroCarreteras/2;
     }
     
+    private void generarCarreteras(){
+        Random random = new Random();
+        //Generamos las carreteras verticales
+        for(int i = 1; i<=this.numCarreteraVer;i++){
+            int longitud = random.nextInt(this.tamaño);
+        }
+    }
     
     
     
